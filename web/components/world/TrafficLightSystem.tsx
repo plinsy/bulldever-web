1e8d6be0787103a1b827cba2a5f3c271351c12b6
Feno Fandresena (Sesena) <gamefandry@gmail.com>
2026-05-10 01:04:18 +0300

    feat: implement dynamic traffic light system with intersection phase management and API routing

--- web/components/world/TrafficLightSystem.tsx
+++ web/components/world/TrafficLightSystem.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useMemo } from "react";
+import { useRef, useMemo, useEffect } from "react";
 import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 import { SCALE, METER } from "../simulation/config";
